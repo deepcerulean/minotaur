1ae8f24727a6803ae3ca68d845d849bc3547afa8
jweissman <joseph.weissman@tapjoy.com>
2012-11-24 22:22:59 -0500

    Update: final cleanups

--- lib/minotaur/extruders/recursive_backtracking_extruder.rb
+++ lib/minotaur/extruders/recursive_backtracking_extruder.rb
@@ -10,6 +10,7 @@ module Minotaur
       #
       def extrude!(opts={})
         start = opts.delete(:start) { origin }
+
         # 'empty' here basically means 'unexplored'
         each_empty_adjacent_to(start) do |next_position|
           build_passage!(start,next_position)
