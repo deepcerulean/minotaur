48196e1420b2cc42fc1d368ce9fcef22198bc7b4
jweissman <joseph.weissman@tapjoy.com>
2012-11-24 21:10:57 -0500

    Cleanup: refactor space somewhat

--- lib/minotaur/geometry/space.rb
+++ lib/minotaur/geometry/space.rb
@@ -29,22 +29,16 @@ module Minotaur
       end
 
       def adjoining_direction(other,inverse=false)
-        if other.x == self.x+self.width #+1
-          if range_overlap?( (y..y+height-1), (other.y..other.y+other.height-1) )
-            return EAST
-          end
-        elsif other.y == self.y+self.height #+1
-          if range_overlap?( (x..x+width-1), (other.x..other.x+other.width-1) )
-            return SOUTH
-          end
+        if adjoining_east?(other)
+          EAST
+        elsif adjoining_south?(other)
+          SOUTH
+        elsif inverse
+          nil
+        else
+          direction_opposite(other.adjoining_direction(self,true))
         end
-
-        # we're exhausted if we were called by ourselves
-        return nil if inverse
-
-        direction_opposite(other.adjoining_direction(self,true))
       end
-
 
       def adjoining?(other_room)
         !adjoining_direction(other_room).nil?
@@ -53,11 +47,9 @@ module Minotaur
       def adjoining_edge(other,inverse=false)
         direction = adjoining_direction(other)
         if direction == SOUTH
-          range = range_overlap(self.x...self.x+width-1, other.x...other.x+other.width-1)
-          range.map { |next_x| [[next_x,other.y-1],[next_x,other.y]] }
+          adjoining_southern_edge(other)
         elsif direction == EAST
-          range = range_overlap(self.y...self.y+height-1, other.y...other.y+other.height-1)
-          range.map { |next_y| [[other.x-1,next_y],[other.x,next_y]] }
+          adjoining_eastern_edge(other)
         else
           inverse ? nil : other.adjoining_edge(self,true)
         end
@@ -65,6 +57,26 @@ module Minotaur
 
       def subdivide(opts={})
         Subdivider.new(opts).subdivide(self,opts)
+      end
+
+      private
+
+      def adjoining_east?(other)
+        other.x == self.x+self.width && range_overlap?( (y..y+height-1), (other.y..other.y+other.height-1) )
+      end
+
+      def adjoining_south?(other)
+        other.y == self.y+self.height && range_overlap?( (x..x+width-1), (other.x..other.x+other.width-1) )
+      end
+
+      def adjoining_southern_edge(other)
+        range = range_overlap(self.x...self.x+width-1, other.x...other.x+other.width-1)
+        range.map { |next_x| [[next_x,other.y-1],[next_x,other.y]] }
+      end
+
+      def adjoining_eastern_edge(other)
+        range = range_overlap(self.y...self.y+height-1, other.y...other.y+other.height-1)
+        range.map { |next_y| [[other.x-1,next_y],[other.x,next_y]] }
       end
     end
   end
